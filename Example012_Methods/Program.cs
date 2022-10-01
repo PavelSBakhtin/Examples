80ab91a3617aa13ae33e963adb7a8e4d7843203c
PavelSB <PavelS@mail.ru>
2022-10-01 19:05:25 +0300

    example 012 methods done

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,45 +1,43 @@
-﻿// // // Вид 1. Описание
+﻿// // // Вид 1. Ничего не принимают, ничего не возвращают
 void Method1() 
 {
     Console.WriteLine("Автор ...");
 }
-// Вызов
-Method1();
+// Вызов:
+// Method1();
 
-// // // Вид2. Описание
+// // // Вид2. Аргументы принимают, ничего не возвращают
 void Method2(string msg)
 {
     Console.WriteLine(msg);
 }
-// Вызов
-Method2(msg: "Текст сообщения");
+// Вызов:
+// Method2("Текст сообщения"); // Не именнованный аргумент
 
-// Описание
+// // Метод с именнованным аргументом (их может быть несколько)
 void Method21(string msg, int count)
 {
     int i = 0;
     while (i < count)
     {
         Console.WriteLine(msg);
-        i++;
+        i++; // инкремент (+), декремент (-)
     }
 }
-// Вызов
-Method21(msg: "Текст", count: 4);
-Method21(count: 4, msg: "новый текст");
+// Вызов:
+// Method21(msg: "Текст", count: 4);
+// Method21(count: 4, msg: "новый текст");
 
-
-// // // Вид 3. Описание
+// // // Вид 3. Ничего не принимают, результат возвращают
 int Method3()
 {
     return DateTime.Now.Year;
 }
-// Вызов
+// Вызов:
 int year = Method3();
-//Console.WriteLine(year);
+// Console.WriteLine(year);
 
-
-// // // Вид4. Описание
+// // // Вид4. Аргументы принимают, результат возвращают
 // string Method4(int count, string text)
 // {
 //    int i = 0;
@@ -53,6 +51,7 @@ int year = Method3();
 //     return result;
 // }
 
+// // Тот же вариант, но с циклом for
 string Method4(int count, string text)
 {
     string result = String.Empty;
@@ -63,35 +62,32 @@ string Method4(int count, string text)
     return result;
 }
 
-string res = Method4(10, "z");
-Console.WriteLine(res);
+// Вызов:
+string res = Method4(4, "xa");
+// Console.WriteLine(res);
 
-//for 
-// // // for (int i = 2; i <= 10; i++)
-// // // {
-// // //     for (int j = 2; j <= 10; j++)
-// // //     {
-// // //         Console.WriteLine($"{i} x {j} = {i * j}");
-// // //     }
-// // //     Console.WriteLine();
-// // // }
 
+// // // Цикл в цикле for 
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}"); // $ - интерполяция строк
+//     }
+//     Console.WriteLine();
+// }
 
 // // // Работа с текстом
-// // // string s = "qwerty"
-// // //             012
-// // // s[3] // r
+// string s = "qwerty";
+// Console.WriteLine(s[3]); // r
 
-// // // Дан текст. В тексте нужно все пробелы заменить черточками, 
-// // // маленькие буквы “к” заменить большими “К”,
-// // // a большие “С” заменить маленькими “с”.
-// // // Ясна ли задача?
+// // Дан текст. В тексте нужно все пробелы заменить черточками, 
+// // маленькие буквы “к” заменить большими “К”.
 
 string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
             + "ежели бы вас послали вместо нашего милого Винценгероде,"
             + "вы бы взяли приступом согласие прусского короля. "
             + "Вы так красноречивы. Вы дадите мне чаю?";
-
 
 string Replace(string text, char oldValue, char newValue)
 {
@@ -109,15 +105,16 @@ string Replace(string text, char oldValue, char newValue)
 
 string newText = Replace(text, ' ', '|');
 
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
+// Вызов:
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
 
-// Сортировка одномерного массива
-int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+// Сортировка одномерного массива методом выбора
+int[] arr = { 8, 5, 3, 4, 2, 6, 7, 1, 9 };
 
-void PrintArray(int[] array)
+void PrintArray(int[] array) // выводит массив на экран
 {
     int count = array.Length;
 
@@ -128,7 +125,7 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-void SelectionSort(int[] array)
+void SelectionSortFromMin(int[] array) // сортирует элементы массива от min до max
 {
     for (int i = 0; i < array.Length - 1 ; i++)
     {
@@ -144,8 +141,24 @@ void SelectionSort(int[] array)
     }
 }
 
+void SelectionSortFromMax(int[] array) // сортирует элементы массива от max до min
+{
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        int maxPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
 
+// Вызов:
 PrintArray(arr);
-SelectionSort(arr);
-
+// SelectionSortFromMin(arr);
+// SelectionSortFromMax(arr);
 PrintArray(arr);
