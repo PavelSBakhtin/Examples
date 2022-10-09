988c7ecd5150d8817aa6b956ba7313b5a28773fc
PavelSB <1987PavelS@mail.ru>
2022-10-10 00:42:08 +0300

    Example 000 Best code done

--- Example000_BestCode/Program.cs
+++ Example000_BestCode/Program.cs
@@ -1,11 +1,27 @@
-﻿string caption = "Intensive C# Devo text";
-int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;
-int screenHeightPosition = Console.WindowHiegth / 2;
+﻿// // Примар хорошего кода (не работает):
 
-DrawText(caption, screenWidthPosition, screenHeightPosition);
+// string caption = "Intensive C# Devo text";
+// int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;
+// int screenHeightPosition = Console.WindowHeigth / 2;
 
-DrawText(
-    text: caption,
-    left: screenWidthPosition,
-    top: screenHeightPosition
+// DrawText(caption, screenWidthPosition, screenHeightPosition);
+
+// string DrawText(
+//     text: caption,
+//     left: screenWidthPosition,
+//     top: screenHeightPosition
+//     );
+
+// // В случайных точках консоли вывести случайные числа ("матрица")
+
+Random r = new Random();
+Console.CursorVisible = false;
+while (true)
+{
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeight)
     );
+    Console.Write(r.Next(10));
+    Thread.Sleep(1000);
+}
