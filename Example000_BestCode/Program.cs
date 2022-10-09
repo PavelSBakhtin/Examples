cd2c62fb7029257340bae12bf92b63d586b9ab1b
PavelSB <1987PavelS@mail.ru>
2022-10-10 00:25:02 +0300

    Example 000 Best code added

---
+++ Example000_BestCode/Program.cs
@@ -0,0 +1,11 @@
+﻿string caption = "Intensive C# Devo text";
+int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;
+int screenHeightPosition = Console.WindowHiegth / 2;
+
+DrawText(caption, screenWidthPosition, screenHeightPosition);
+
+DrawText(
+    text: caption,
+    left: screenWidthPosition,
+    top: screenHeightPosition
+    );
